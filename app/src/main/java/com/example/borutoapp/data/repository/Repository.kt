aace78d6310e035f1a5496d33940dca237ef7821
yashanshav <yashanshavvrajpurohit@gmail.com>
2022-07-24 22:49:46 +0530

    implemented remoteDataSource

--- app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
@@ -1,12 +1,21 @@
 package com.example.borutoapp.data.repository
 
+import androidx.paging.PagingData
+import com.example.borutoapp.domain.models.Hero
 import com.example.borutoapp.domain.repository.DataStoreOperations
+import com.example.borutoapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
     private val dataStore: DataStoreOperations
 ) {
+
+    fun getAllHeroes(): Flow<PagingData<Hero>> {
+        return remoteDataSource.getAllHeroes()
+    }
+
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed)
     }
--- app/src/main/java/com/example/borutoapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/borutoapp/di/NetworkModule.kt
@@ -1,14 +1,19 @@
 package com.example.borutoapp.di
 
 import android.provider.MediaStore
+import androidx.paging.ExperimentalPagingApi
+import com.example.borutoapp.data.local.BorutoDatabase
 import com.example.borutoapp.data.remote.BorutoApi
+import com.example.borutoapp.data.repository.RemoteDataSourceImpl
 import com.example.borutoapp.domain.models.ApiResponse
+import com.example.borutoapp.domain.repository.RemoteDataSource
 import com.example.borutoapp.util.constants.BASE_URL
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import retrofit2.Retrofit
@@ -16,6 +21,8 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 import okhttp3.OkHttpClient as OkHttpClient
 
+@ExperimentalPagingApi
+@ExperimentalSerializationApi
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
@@ -44,5 +51,17 @@ object NetworkModule {
     @Singleton
     fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
         return retrofit.create(BorutoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(
+        borutoApi: BorutoApi,
+        borutoDatabase: BorutoDatabase
+    ): RemoteDataSource {
+        return RemoteDataSourceImpl(
+            borutoApi = borutoApi,
+            borutoDatabase = borutoDatabase
+        )
     }
 }
