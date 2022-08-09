c6339f9d547633c6abc967ff258b2bf1aace83f7
yashanshav <yashanshavvrajpurohit@gmail.com>
2022-08-09 07:41:55 +0530

    completed app

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.borutoapp.util
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.29.176:52737"
+    const val BASE_URL = "https://yashanshav-boruto-server.herokuapp.com/"
 
     const val DETAILS_ARGUMENTS_KEY = "heroId"
 
